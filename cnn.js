1612c8fbc7022eac6e0eafa678a333df43806ed7
Moremi Adekunle <adekunleomoremi@gmail.com>
2025-11-10 14:35:05 +0100

    fix the api errors

--- cnn.js
+++ cnn.js
@@ -10,11 +10,6 @@ const url =  `https://newsapi.org/v2/everything?q=tesla&pageSize=10&sortBy=popul
 
 async function cableNews() {
     try{
-        // const author = document.getElementById('authorName');
-        // const time = document.getElementById('time');
-        // const image = document.getElementById('image');
-        // const link = document.getElementById('desc')
-
         const response = await fetch(url);
 
         if(!response){
@@ -70,53 +65,13 @@ async function cableNews() {
             background.classList.add('background');
             row.appendChild(background);
 
-
             row.appendChild(newsItemContainer);
 
-            // const background = document.getElementById('background');
-            // background.classList.add('background')
-            // row.appendChild(background)
-
-
-
-
-
-        
-            // for(let item of article){
-
-                // author.innerHTML = article.author
-                // console.log('article' , article)
-            // }
-
-
-            // <div class="news-item news-item-media1 d-flex col-md-6 d-md-block col-lg-4">
-            //     <div class="news-item-img">
-            //         <img src="images/newsImg_1.jpg" alt="" id="image">
-            //     </div>
-            //     <div class="news-item-content">
-            //         <h3 class="pb-md-1 pt-md-2 " id="authorName">The Verge</h3>
-            //         <p> <a href="" id="desc">
-            //             The Verge's website homepage is vibrant
-            //             a black and white theme with bright accents of orange and magenta</p>
-            //         </a>
-            //         <div class="time" id="time">
-            //             <span class="mee">2hours ago</span>
-            
-            //             <span>Aug15,2025</span>
-            //         </div>
-            //     </div>
-            // </div>
-            
         });
-        console.log('row' , row)
-       
-        // author.textContent = responseData.articles[1].author;
-        
-
+        console.log('row' , row)        
     }
     catch(error){
         console.log('error', error)
-
     }
 }
 cableNews()
